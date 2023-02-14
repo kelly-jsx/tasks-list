ad4c1b730c92bd905d7e27578fd65224f7c0aeb7
Kelly <50586093+kelly-jsx@users.noreply.github.com>
2023-02-14 16:58:36 +0100

    saving tasks to LocalStorage

--- src/components/TaskModal.tsx
+++ src/components/TaskModal.tsx
@@ -1,12 +1,41 @@
 import type { ReactElement } from 'react'
 import { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
 
 export default function TaskModal(): ReactElement {
+	const [existingTasks, setExistingTasks] = useState(
+		JSON.parse(localStorage.getItem('tasks') || '[]')
+	)
+
 	const [task, setTask] = useState({
+		id: '',
 		taskName: '',
 		description: '',
-		category: ''
+		category: '',
+		important: false,
+		completed: false
 	})
+
+	const handleClick = () => {
+		const newTask = {
+			id: uuidv4(),
+			taskName: task.taskName,
+			description: task.description,
+			category: task.category || 'main',
+			important: task.important,
+			completed: task.completed
+		}
+		setExistingTasks(previousTasks => [...previousTasks, newTask])
+		localStorage.setItem('tasks', JSON.stringify([...existingTasks, newTask]))
+		setTask({
+			id: '',
+			taskName: '',
+			description: '',
+			category: '',
+			important: false,
+			completed: false
+		})
+	}
 
 	const handleInputChange = e => {
 		const { name, value } = e.target
@@ -14,11 +43,6 @@ export default function TaskModal(): ReactElement {
 			...previousProperties,
 			[name]: value
 		}))
-	}
-
-	const handleSubmit = (e: { preventDefault: () => void }) => {
-		e.preventDefault()
-		console.log('test')
 	}
 
 	return (
@@ -30,25 +54,57 @@ export default function TaskModal(): ReactElement {
 						type='text'
 						placeholder='Task title'
 						className='input input-bordered w-full max-w-sm'
+						name='taskName'
+						value={task.taskName}
+						onChange={handleInputChange}
 					/>
 					<textarea
 						className='textarea textarea-bordered mt-2 w-full max-w-sm'
 						placeholder='Description (optional)'
+						name='description'
+						value={task.description}
+						onChange={handleInputChange}
 					/>
 					<div className='w-full max-w-sm'>
 						<label className='label'>
 							<span className='label-text'>Category</span>
 						</label>
-						<select className='select select-bordered w-full max-w-sm'>
-							<option>Main</option>
+						<select
+							className='select select-bordered w-full max-w-sm'
+							name='category'
+							value={task.category}
+							onChange={handleInputChange}
+						>
+							<option value='main'>Main</option>
+							<option value='secondary'>Secondary</option>
 						</select>
 					</div>
 					<label className='label mt-1 w-full max-w-sm cursor-pointer place-content-start gap-2'>
-						<input type='checkbox' className='checkbox checkbox-primary m-0' />
+						<input
+							type='checkbox'
+							className='checkbox checkbox-primary m-0'
+							name='important'
+							value={task.important}
+							onChange={() =>
+								setTask(previousProperties => ({
+									...previousProperties,
+									important: !previousProperties.important
+								}))
+							}
+						/>
 						<span className='label-text'>Mark as important</span>
 					</label>
 					<label className='label w-full max-w-sm cursor-pointer place-content-start gap-2'>
-						<input type='checkbox' className='checkbox checkbox-primary m-0' />
+						<input
+							type='checkbox'
+							className='checkbox checkbox-primary m-0'
+							onChange={() =>
+								setTask(previousProperties => ({
+									...previousProperties,
+									completed: !previousProperties.completed
+								}))
+							}
+						/>
 						<span className='label-text'>Mark as completed</span>
 					</label>
 				</div>
@@ -56,45 +112,11 @@ export default function TaskModal(): ReactElement {
 					<a href='#' className='btn btn-ghost'>
 						Cancel
 					</a>
-					<a href='#' className='btn btn-primary'>
+					<a href='#' className='btn btn-primary' onClick={handleClick}>
 						Add Task
 					</a>
 				</div>
 			</div>
 		</div>
-
-		// <>
-		// 	<input type='checkbox' id='task-modal' className='modal-toggle' />
-		// 	<div className='modal'>
-		// 		<div className='modal-box'>
-		// 			<form action='#' onSubmit={handleSubmit}>
-		// 				<h3 className='text-lg font-bold'>Add new task</h3>
-		// 				<div className='flex flex-col'>
-		// 					<input type='text' />
-		// 				</div>
-
-		// 				<div className='modal-action mt-4'>
-		// 					<label htmlFor='task-modal' className='btn btn-ghost'>
-		// 						Cancel
-		// 					</label>
-		// 					<button
-		// 						type='submit'
-		// 						className='btn btn-primary'
-		// 						htmlFor='task-modal'
-		// 					>
-		// 						Add
-		// 					</button>
-		// 					<label
-		// 						htmlFor='task-modal'
-		// 						className='btn btn-primary'
-		// 						type='submit'
-		// 					>
-		// 						Add Task
-		// 					</label>
-		// 				</div>
-		// 			</form>
-		// 		</div>
-		// 	</div>
-		// </>
 	)
 }
